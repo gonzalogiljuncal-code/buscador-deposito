8486e2b1bee8d103c38b24f15254eceb56a374ee
gonzalogiljuncal-code <gonzalo.giljuncal@johnfoos.com>
2026-04-24 13:49:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.set_page_config(page_title="Buscador de Pasillos", page_icon="👟")
+st.set_page_config(page_title="Buscador de Pasillos John Foos", page_icon="👟")
 
-st.title("👟 Buscador de Pasillos")
+st.title("👟 Buscador de Pasillos JF")
 st.markdown("Ingresá el modelo para saber su ubicación.")
 
 @st.cache_data
